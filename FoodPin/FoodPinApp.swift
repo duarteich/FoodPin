b7ec4c9823b7588f28f4526b1454520ae4b5093a
Christyan Duarte <duarteich@gmail.com>
2023-07-03 21:23:38 -0600

    Initial commit

---
+++ FoodPin/FoodPinApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodPinApp.swift
+//  FoodPin
+//
+//  Created by Christyan Duarte on 28/06/23.
+//
+
+import SwiftUI
+
+@main
+struct FoodPinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            RestaurantListView()
+        }
+    }
+}
